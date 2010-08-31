e65900c92ab1e9c8b2f79e5f23e855962dbfc211
sarnowski <sarnowski@caa2ac01-1b1e-4351-a100-d44f7e30510d>
2010-08-31 19:32:01 +0000

    more basic features added and some kind of documentation ;-)

--- src/main/java/de/cosmocode/issuetracker/Issue.java
+++ src/main/java/de/cosmocode/issuetracker/Issue.java
@@ -1,18 +1,69 @@
+/**
+ * Copyright 2010 CosmoCode GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.cosmocode.issuetracker;
 
 /**
+ * Describes a single issue of an issue tracker.
+ *
  * @author Tobias Sarnowski
  */
 public interface Issue {
 
+    /**
+     * Returns a unique ID, which will be used to identify
+     * the issue in the source issue tracker.
+     *
+     * @return a unique ID
+     */
     String getId();
 
+    /**
+     * The source issue tracker where the issue comes from.
+     *
+     * @return source issue tracker
+     */
+    IssueTracker getIssueTracker();
+
+    /**
+     * Returns the issue's title.
+     *
+     * @return issue's title
+     */
     String getTitle();
 
+    /**
+     * Sets the issue's title
+     *
+     * @param title issue's title
+     */
     void setTitle(String title);
 
+    /**
+     * Returns the issue's description
+     *
+     * @return issue's description
+     */
     String getDescription();
 
+    /**
+     * Sets the issue's description
+     *
+     * @param description issue's description
+     */
     void setDescription(String description);
 
 }
--- src/main/java/de/cosmocode/issuetracker/IssueTracker.java
+++ src/main/java/de/cosmocode/issuetracker/IssueTracker.java
@@ -1,10 +1,56 @@
+/**
+ * Copyright 2010 CosmoCode GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.cosmocode.issuetracker;
 
+import java.util.List;
+
 /**
+ * Describes a generic issue tracker like JIRA,
+ * ActiveCollab, BugZilla, ... or self made issue
+ * tracking.
+ *
  * @author Tobias Sarnowski
  */
 public interface IssueTracker {
 
+    /**
+     * Creates a new issue.
+     *
+     * @param title the issue's title
+     * @param description the issue's description
+     * @return the created issue
+     */
     Issue createIssue(String title, String description);
+
+    /**
+     * Lists all issues. Depends on the implementation, which
+     * issues will be returned.
+     *
+     * @return a list of issues
+     */
+    List<Issue> listIssues();
+
+    /**
+     * Updates an issue in the tracker. The issue to update
+     * is identified by the
+     * {@link de.cosmocode.issuetracker.Issue#getId()}.
+     *
+     * @param issue the source issue for the update
+     */
+    void updateIssue(Issue issue);
 
 }
